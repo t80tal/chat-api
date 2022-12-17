185093269126f1b8ec5201e7a9ded80f2b0e45c6
Tal <tal@gitrak.com>
2022-12-17 19:38:30 +0200

    Fixed cors

--- api/settings.py
+++ api/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
     "user",
     "message",
 ]
@@ -44,7 +45,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "api.urls"
 
